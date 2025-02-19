ffaa379c81e3aa4f22e6a5ca57f0b5209fbaba49
nithishkumar189704 <nithishkumar189704@gmail.com>
2025-02-19 15:01:48 +0530

    ecommerce_website

--- ec/app/views.py
+++ ec/app/views.py
@@ -2,6 +2,7 @@ from django.db.models import Count
 from django.shortcuts import render
 from django.views import View
 from . models import Product
+from . forms import CustomerRegistrationForm
 
 # Create your views here.
 
@@ -32,3 +33,9 @@ class ProductDetail(View):
     def get(self, request,pk):
         product = Product.objects.get(pk=pk)
         return render(request, "app/productdetail.html", locals())    
+    
+
+class CustomerRegistrationView(View):
+    def get(self,request):
+        form =  CustomerRegistrationForm()
+        return render(request, 'app/customerregistration.html',locals())
