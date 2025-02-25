8b2ddabebfadc4611868f2b5c01966855ba3a0a3
nithishkumar189704 <nithishkumar189704@gmail.com>
2025-02-25 13:24:41 +0530

    updated

--- ec/app/views.py
+++ ec/app/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Count
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from . models import Product
 from . forms import CustomerRegistrationForm, CustomerProfileForm,Customer
@@ -79,8 +79,21 @@ def address(request):
 
 class updateAddress(View):
     def get(self,request,pk):
-        form = CustomerProfileForm()
+        add = Customer.objects.get(pk=pk)
+        form = CustomerProfileForm(instance=add)
         return render(request, 'app/updateAddress.html',locals())
     def post(self,request,pk):
         form = CustomerProfileForm(request.POST)
-        return render(request, 'app/updateAddress.html',locals())
+        if form.is_valid():
+            add = Customer.objects.get(pk=pk)
+            add.name = form.cleaned_data['name']
+            add.locality = form.cleaned_data['locality']
+            add.city = form.cleaned_data['city']
+            add.mobile = form.cleaned_data['mobile']
+            add.state = form.cleaned_data['state']
+            add.zipcode = form.cleaned_data['zipcode']
+            add.save()
+            messages.success(request, "Congratulations! Profile Updated Successfully")
+        else:
+            messages.warning(request, "Invalid Input Data")
+        return redirect("address")
